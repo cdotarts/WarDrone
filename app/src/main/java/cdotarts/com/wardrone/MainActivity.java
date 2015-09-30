717d9260e6bb614477cf66ba59a28f2055c53d96
cdotarts <morechaco@gmail.com>
2015-09-30 20:30:59 +0530

    removed fragment interaction interface

--- app/src/main/java/cdotarts/com/wardrone/MainActivity.java
+++ app/src/main/java/cdotarts/com/wardrone/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Dashboard"));
+        tabLayout.addTab(tabLayout.newTab().setText("Shirts"));
+
 
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         final PagerAdapter adapter = new PagerAdapter
